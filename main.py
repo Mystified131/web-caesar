7a326d99f36324140e1d7f0451d2d7227dfa8e80
Thomas Jackson Park <mystifiedthomas@gmail.com>
2018-09-08 11:19:51 -0500

    Almost finished

--- main.py
+++ main.py
@@ -5,40 +5,43 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
+
 def index():
     return """
-<form method = 'POST'>
-<!DOCTYPE html>
+    <form method = 'POST'>
+    <!DOCTYPE html>
 
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
             }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }
-        </style>
-    </head>
-    <body>
-        Rotate by: <input type = "text" name = "rot" value = "0">
-        <br><br>
-        <textarea name = "text"></textarea>
-        <br><br>
-        <input type="submit" />
-    </body>
-</html>
+                textarea {{
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }}
+            </style>
+        </head>
+        <body>
+            Rotate by: <input type = "text" name = "rot" value = "0">
+            <br><br>
+            <textarea name = "text">{0}</textarea>
+            <br><br>
+            <input type="submit" />
+        </body>
+    </html>
 
-</form>
-"""
+    </form>
+
+    """
+
 @app.route("/", methods=['POST'])
 
 def encrypt():
